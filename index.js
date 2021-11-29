e7d514faacc69e128943bffa0f06ed404281d311
Austin <austin.j.fitzhugh@gmail.com>
2021-11-29 09:56:43 -0500

    commit tree a2408552183e945f0b5159b5b82717296c23f6da

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function superbowlWin(record){
+    function isWin(record){
+        return record.result === 'W';
+    }
 
+
+    let result = undefined; 
+    for(let item of record){
+        if(isWin(item) === true){
+            result = item.year;
+            break;
+        }
+    }
+    return result;
+}
